8c6aaa9e112af675a3a8921f01eaeaf73fe13cc8
juliandavid-garcia <juliandavid.garcia@gmail.com>
2021-10-06 13:36:09 +0000

    added images and collapsible boxes to the categories

--- app.py
+++ app.py
@@ -19,6 +19,22 @@ def index():
     return render_template("index.html")
 
 
+
+@app.route("/login")
+def login(): 
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout(): 
+    return render_template("logout.html")
+
+
+@app.route("/register")
+def register(): 
+    return render_template("register.html")
+
+
 if __name__=="__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),

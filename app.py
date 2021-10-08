50464af1a7ce7b16798c5d2c9f3b249594935255
juliandavid-garcia <juliandavid.garcia@gmail.com>
2021-10-08 13:47:23 +0000

    Bind data from db to accordion

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_cocktails")
 def get_cocktails(): 
-    cocktails = mongo.db.cocktails.find()
+    cocktails = list(mongo.db.cocktails.find())
     return render_template("index.html", cocktails=cocktails)
 
 
